6c0364ef98cb2d6f327fc76102e23bb2d15c7ac3
Jason Hubbard <jasonahubbard@gmail.com>
2021-01-19 09:50:39 -0500

    setup teardown

--- tests/unit/sample_test.py
+++ tests/unit/sample_test.py
@@ -7,7 +7,7 @@ from pyspark.sql import SparkSession
 class SampleJobUnitTest(unittest.TestCase):
     def setUp(self):
         # initialze stuff here
-        println("setup")
+        print("setup")
 
     def test_sample(self):
         output_count = 10
@@ -15,7 +15,7 @@ class SampleJobUnitTest(unittest.TestCase):
 
     def tearDown(self):
         # uninitialize here
-        println("tearDown")
+        print("tearDown")
 
 if __name__ == "__main__":
     unittest.main()

491573b701d4c0fa075876930a04a6ff3e5a1752
Jason Hubbard <jasonahubbard@gmail.com>
2021-01-19 09:38:29 -0500

    fix test

--- tests/unit/sample_test.py
+++ tests/unit/sample_test.py
@@ -6,11 +6,6 @@ from pyspark.sql import SparkSession
 
 class SampleJobUnitTest(unittest.TestCase):
     def setUp(self):
-        self.test_dir = tempfile.TemporaryDirectory().name
-        self.test_config = {
-            "output_format": "parquet",
-            "output_path": os.path.join(self.test_dir, "output")
-        }
 
 
     def test_sample(self):
@@ -20,7 +15,6 @@ class SampleJobUnitTest(unittest.TestCase):
         self.assertGreater(output_count, 0)
 
     def tearDown(self):
-        shutil.rmtree(self.test_dir)
 
 if __name__ == "__main__":
     unittest.main()
